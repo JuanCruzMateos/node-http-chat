b0ad7df34afd34a2a04c2653ea49559876d735e6
JuanCruzMateos <juanczmt@gmail.com>
2021-12-28 16:16:23 -0300

    update

---
+++ src/repository/test.js
@@ -0,0 +1,7 @@
+const users = require('./user-repository');
+
+
+users.saveUser("juan", "juan@gmail.com", "21");
+users.saveUser("juan", "juan@gmail.com", "21");
+users.saveUser("juan", "juan@gmail.com", "21");
+users.saveUser("juan", "juan@gmail.com", "21");
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,5 +1,39 @@
-const users = require('./active-users.json')
+const fs = require('fs');
+const FILENAME = "./active-users.json";
 
-for (let user of users) {
-    console.log(user);
+let users;
+
+fs.readFile(FILENAME, "utf-8", (error, data) => {
+    if (error) {
+        console.error(error);
+    } else {
+        users = JSON.parse(data);
+        console.log(users);
+    }
+});
+
+const existsUser = email => {
+    return Boolean(users.find(user => user.email === email));
+}
+
+const saveUser = (name, email, roomId) => {
+    users.push({ name, email, roomId });
+    fs.writeFile(FILENAME, JSON.stringify(users), (error) => {
+        if (error)
+            throw error;
+    });
+}
+
+const deleteUser = email => {
+    users = users.filter(user => user.email !== email);
+    fs.writeFile(FILENAME, JSON.stringify(users), (error) => {
+        if (error)
+            throw error;
+    });
+}
+
+module.exports = {
+    existsUser,
+    saveUser,
+    deleteUser
 }
